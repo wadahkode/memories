95b8bd73181581db44a0fd0280ce1362599350c3
wadahkode <mvp.dedefilaras@gmail.com>
2021-02-11 08:29:52 +0700

    configuration for umd

--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,25 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    memories: "./index.js",
+    memories: './index.js',
   },
+  target: 'web',
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
   ],
   output: {
-    filename: "[name].min.js",
-    path: path.resolve(__dirname, "build"),
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'build'),
+    library: 'Memories',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    // hotUpdateChunkFilename: '[memories].[fullhash].hot-update.js',
   },
   performance: {
-    hints: "warning",
+    hints: 'warning',
     //maxEntrypointSize: 4 * 1024 * 1024,
     //maxAssetSize: 10 * 1024 * 1024
   },
