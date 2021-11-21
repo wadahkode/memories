272846e133134da604118bd7945d5024f649413d
wadahkode <mvp.dedefilaras@gmail.com>
2021-11-21 23:01:34 +0700

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.1.82',
+        'version' => '1.1.82.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'wadahkode/memories',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v201',
+            'version' => '201.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'reference' => 'aa5b19de5c45a32af27c87d655f5229b155fce25',
+            'dev_requirement' => true,
+        ),
+        'wadahkode/memories' => array(
+            'pretty_version' => '1.1.82',
+            'version' => '1.1.82.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
